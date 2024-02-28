23bababe27a9c78bb35a98b3ceba1f3628b99bd3
shab0204 <sharipova.shabnam02@gmail.com>
2024-02-28 17:51:06 +0600

    frst

---
+++ main.js
@@ -0,0 +1,52 @@
+
+// for ( let i= 1 ; i <= 100;i++){
+//     console.log(i);
+// }
+
+// for ( let i= -100 ; i <= 1; i++){
+//     console.log(i);
+// }
+// for (let i = 100; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// for(let i= 1 ; i <= 100;i++){
+//     if(i % 2 === 0)
+//     console.log(i);
+// }
+// for(let i= 1 ; i <= 100;i++){
+//     if(i % 3 === 0)
+//     console.log(i);
+// }
+// let numbersPush = []
+// for ( let i= 1 ; i <= 100;i++){
+//     console.log(numbersPush.push(i));
+// }
+let numbersPush = []
+// for(let i= 2 ; i <= 100; i += 2 ){
+  
+//      numbersPush.push(i)
+//      console.log((i));
+for(let i =1 ;i<=100;i++){
+    if(i % 2===0){
+        numbersPush.push(i)
+        console.log(numbersPush);
+    }
+}
+    
+// }
+// let numbersPush = []
+// for ( let i= 1 ; i <= 10; i++){
+//         console.log(numbersPush.push(i));
+//     }
+// let numbers = [ 2,3,-4,-5,8,-7,-9,78,-4]
+// let numbersNegative = numbers.filter(number => number < 0)
+// const numbersCount = numbersNegative.length
+// console.log(numbersCount);
+// let numbers = [ 2,3,-4,-5,8,-7,-9,78,-4]
+// let positivNumbers = numbers.filter(number => number > 0)
+// console.log(positivNumbers);
+
+// let string = 'year'
+// string = string.slice(0,-2,)+string.slice(-1)
+// console.log(string);
